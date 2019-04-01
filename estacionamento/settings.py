965897e7471103a8a60267e38b1fa613a6425eaf
Paulo W. Limaverde <vorfast@gmail.com>
2019-04-01 00:36:24 -0300

    correcao host

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.1.10', 'parkinglimaverde.herokuapp.com']
+ALLOWED_HOSTS = ['192.168.1.10', 'estacionamentolimaverde.herokuapp.com']
 
 # Application definition
 
