bf7e96db811671e42b6a8f5f9ef0be92f3c7f2eb
Paulo W. Limaverde <vorfast@gmail.com>
2019-04-11 02:19:22 -0300

    ajuste links website

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import(
+    home,
+    contato,
+    planos,
+    servicos,
+    sobre
+)
 
 urlpatterns = [
-    path('', home,
-         name='home'),
+    path('', home, name='home'),
+    path('contato/', contato, name='contato'),
+    path('planos/', planos, name='planos'),
+    path('servicos/', servicos, name='servicos'),
+    path('sobre/', sobre, name='sobre'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'website/index.html')
+
+
+def contato(request):
+    return render(request, 'website/contato.html')
+
+
+def planos(request):
+    return render(request, 'website/planos.html')
+
+
+def servicos(request):
+    return render(request, 'website/servicos.html')
+
+
+def sobre(request):
+    return render(request, 'website/sobre.html')
